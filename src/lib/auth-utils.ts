a3107a678a150368ca8cf979e9100e19cc3061a4
lschneiderny <163238998+lschneiderny@users.noreply.github.com>
2025-09-29 10:02:23 -0400

    redo

--- src/lib/auth-utils.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from "@prisma/client"
-
-export function isEmployeeOrAdmin(role: UserRole | undefined): boolean {
-  return role === UserRole.EMPLOYEE || role === UserRole.ADMIN
-}
-
-export function isAdmin(role: UserRole | undefined): boolean {
-  return role === UserRole.ADMIN
-}
